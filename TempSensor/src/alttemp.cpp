a8a308b4bb212433b1402e6fa677f45ebd2dde99
André Biseth <andre@biseth.net>
2019-03-05 22:36:03 +0100

    Changed variable name

--- TempSensor/src/alttemp.cpp
+++ TempSensor/src/alttemp.cpp
@@ -10,19 +10,19 @@ AltTemp::~AltTemp()
 
 void AltTemp::setup()
 {
-  _tempData1.beta = (log(_tempData1.RT1 / _tempData1.RT2)) / ((1 / _tempData1.T1) - (1 / _tempData1.T2));
-  _tempData1.Rinf = _tempData1.R0 * exp(-_tempData1.beta / _tempData1.T0);
+  _tempData.beta = (log(_tempData.RT1 / _tempData.RT2)) / ((1 / _tempData.T1) - (1 / _tempData.T2));
+  _tempData.Rinf = _tempData.R0 * exp(-_tempData.beta / _tempData.T0);
 }
 
 float AltTemp::getTemperature(){
   // Taken from:
   // https://www.instructables.com/id/NTC-Temperature-Sensor-With-Arduino/
   
-  _tempData1.Vout = _tempData1.Vin * ((float)(analogRead(0)) / 1024.0); // calc for ntc
-  _tempData1.Rout = (_tempData1.Rt * _tempData1.Vout / (_tempData1.Vin - _tempData1.Vout));
+  _tempData.Vout = _tempData.Vin * ((float)(analogRead(0)) / 1024.0); // calc for ntc
+  _tempData.Rout = (_tempData.Rt * _tempData.Vout / (_tempData.Vin - _tempData.Vout));
 
-  _tempData1.TempK = (_tempData1.beta / log(_tempData1.Rout / _tempData1.Rinf)); // calc for temperature
-  _tempData1.TempC = _tempData1.TempK - 273.15;
+  _tempData.TempK = (_tempData.beta / log(_tempData.Rout / _tempData.Rinf)); // calc for temperature
+  _tempData.TempC = _tempData.TempK - 273.15;
 
-  return _tempData1.TempC;
+  return _tempData.TempC;
 }
